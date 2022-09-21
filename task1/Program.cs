6b24f39d88a036d8279de0b67a7bb2d870cb1c82
Vera <vera.serebryakova.24@mail.ru>
2022-09-22 01:10:17 +0300

    hw6

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿bool identifyNums(string[] arrNums)
+{
+    bool flag = true; 
+    for (int i = 0; i < arrNums.Length; i++)
+    {
+        if ((int.TryParse(arrNums[i], out int NumInt) | double.TryParse(arrNums[i], System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double NumDouble)) == false)
+            {
+                Console.WriteLine("Введите через запятую ТОЛЬКО числа:");
+                flag = false;                
+                break;
+            }
+    }
+    return flag;
+}
+
+int countPosNums(string[] arrC)
+{
+    int count = 0;
+    for (int iArrC =0; iArrC < arrC.Length; iArrC++)
+    {
+        if ((int.TryParse(arrC[iArrC], out int NumInt) == true) && (NumInt > 0))
+        {
+            count++;
+        }
+        if ((double.TryParse(arrC[iArrC], System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double NumDouble) == true) && (NumDouble > 0))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] arr;
+Console.WriteLine("Введите через запятую несколько чисел:");
+do
+{
+    string strIn = Convert.ToString(Console.ReadLine());
+    arr = strIn.Replace(" ", "").Split(new char[] {','});
+}
+while(identifyNums(arr) == false);
+Console.Write(countPosNums(arr));
---
+++ task2/Program.cs
@@ -0,0 +1,48 @@
+﻿double[] findDot(double[,] coeffArr)
+{
+    double[] dot = new double[2];
+    dot[0] = Math.Round((coeffArr[1,1]-coeffArr[0,1])/(coeffArr[0,0]-coeffArr[1,0]), 2);
+    dot[1] = Math.Round(coeffArr[0,0] * dot[0] + coeffArr[0,1], 2);
+    return dot;
+}
+
+double[,] inputCoeffs()
+{
+    double[,] coeffs = new double[2,2];
+    string symCoeff = "";
+    //[[k1,b1], [k2,b2]]
+    for (int i = 0; i < coeffs.GetLength(0); i++)
+    {
+        for (int j = 0; j < coeffs.GetLength(1); j++)
+        {
+            if (j == 0) symCoeff = "k";
+            if (j == 1) symCoeff = "b";
+            Console.Write($"Введите коэффициент {symCoeff}{i+1}: ");
+            coeffs[i,j] = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    return coeffs;
+}
+
+void printRightConds(string LineState)
+{
+    Console.WriteLine($"{LineState}Введите коэффициенты удовлетворяющие условиям:\n 1) Прямые не совпадают (Условие совпадения прямых: k1 = k2 и b1 = b2) \n 2) Прямые не параллельны (Условие паралельности прямых: k1 = k2 и b1 != b2)");
+}
+
+void checkLines(double[,] coeffLines)
+{
+    if ((coeffLines[0,0] == coeffLines[1,0]) && (coeffLines[0,1] == coeffLines[1,1])) 
+    {
+        printRightConds("Прямые совпадают! ");
+        checkLines(inputCoeffs());
+    }
+    else if ((coeffLines[0,0] == coeffLines[1,0]) && (coeffLines[0,1] != coeffLines[1,1])) 
+    {
+        printRightConds("Прямые параллельны! ");
+        checkLines(inputCoeffs());
+    }
+    else
+    Console.WriteLine("Координаты точки пересечения: ({0})",String.Join("; ",findDot(coeffLines)));
+} 
+
+checkLines(inputCoeffs());
